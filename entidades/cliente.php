02b177cf57c6d0a86919b8d6437ed7b5e8ad6988
lucasglr <lucas-glr@hotmail.com>
2020-08-08 13:35:57 -0300

    cambio

--- entidades/cliente.php
+++ entidades/cliente.php
@@ -127,7 +127,7 @@ class Cliente {
         idcliente DESC";
         $resultado = $mysqli->query($sql);
 
-        if($resultado){
+        if($resultado){https://github.com/lucasglr/AMBVenta.git
             while ($fila = $resultado->fetch_assoc()) {
                 $obj = new Cliente();
                 $obj->idcliente = $fila["idcliente"];
