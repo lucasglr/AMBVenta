b8d4e0912ece57d1805a816cc7e77fcab3cafefa
lucasglr <lucas-glr@hotmail.com>
2020-10-26 10:23:38 -0300

    cambio

--- config.php
+++ config.php
@@ -9,7 +9,7 @@ class Config {
     const BBDD_HOST = "127.0.0.1"; //127.0.0.1
     const BBDD_USUARIO = "root"; //root
     const BBDD_CLAVE = ""; // vacio
-    const BBDD_NOMBRE = ""; //abmventas
+    const BBDD_NOMBRE = "abmventas"; //abmventas
 }
 
 
